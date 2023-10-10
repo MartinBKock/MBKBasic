53c018954cd7078915f0d158924396ae9e8c2a81
Martin Kock <Martin_kock@me.com>
2023-10-10 17:22:25 +0200

    added a photoLibraryContainer

--- Sources/MBKBasic/UIViewRepresentable.swift
+++ Sources/MBKBasic/UIViewRepresentable.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
       @State var color = Color.red
       @State var text = ""
       @State var sliderVal: Float = 0.5
+      @State var photoSheetIsPresented = false
+      @State var image: UIImage?
+      
       var body: some View {
             
          
@@ -39,6 +42,23 @@ struct ContentView: View {
                   Slider(value: $sliderVal)
                   UISliderWithRepresentable(sliderVal: $sliderVal)
                   Text("\(sliderVal)")
+                  
+                  // PhotoLibrary
+                  Text("Photo Library")
+                  HStack {
+                        Button(action: {
+                              photoSheetIsPresented.toggle()
+                        }, label: {
+                              /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                        })
+                  }.sheet(isPresented: $photoSheetIsPresented, content: {
+                        UIPhotoLibraryWithRepresentable(isPresented: $photoSheetIsPresented, selectedImage: $image)
+                  })
+                  Image(uiImage: image ?? UIImage())
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                  
             }
       }
 }
@@ -173,4 +193,44 @@ struct UISliderWithRepresentable: UIViewRepresentable {
       
       
 }
+
+struct UIPhotoLibraryWithRepresentable: UIViewControllerRepresentable {
+      @Binding var isPresented: Bool
+      @Binding var selectedImage: UIImage?
+      
+      func makeUIViewController(context: Context) -> UIImagePickerController {
+            let picker = UIImagePickerController()
+            picker.delegate = context.coordinator
+            return picker
+      }
+      
+      func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+            
+      }
+      
+      func makeCoordinator() -> Coordinator {
+            Coordinator(self)
+      }
+      
+      class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+            let parent: UIPhotoLibraryWithRepresentable
+            
+            init(_ parent: UIPhotoLibraryWithRepresentable) {
+                  self.parent = parent
+            }
+            
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                  if let image = info[.originalImage] as? UIImage {
+                        parent.selectedImage = image
+                  }
+                  
+                  parent.isPresented = false
+            }
+            
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                  parent.isPresented = false
+            }
+      }
+}
+
 
