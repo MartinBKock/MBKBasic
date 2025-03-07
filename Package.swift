157b4476ca2ab4515f14810c42c981d794ff3b7b
MartinBKock <martin_kock@me.com>
2025-03-07 16:45:03 +0100

    removed error

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
       name: "MBKBasic",
       platforms: [
-            .iOS(.v16)
+            .iOS(.v16),
       ],
       products: [
             .library(
@@ -14,14 +14,14 @@ let package = Package(
                   targets: ["MBKBasic"]),
       ],
       dependencies: [
-        .package(url: "https://github.com/MartinBKock/mbkError.git", branch: "develop"),
+//        .package(url: "https://github.com/MartinBKock/mbkError.git", branch: "develop"),
         .package(url: "https://github.com/Alamofire/Alamofire", branch: "master"),
       ],
       targets: [
             .target(
                   name: "MBKBasic",
                   dependencies: [
-                    .product(name: "MBKError", package: "MBKError"),
+//                    .product(name: "MBKError", package: "MBKError"),
                     .product(name: "Alamofire", package: "Alamofire"),
                   ]),
             .testTarget(
